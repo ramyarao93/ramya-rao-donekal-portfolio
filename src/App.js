45e1740351711d28807ae349ba11ac0bac0c941f
ramya_rao <ramya.donekal@itpeoplecorp.com>
2021-04-12 11:16:17 -0400

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./About";
+import Skills from "./Skills";
+import Experience from "./Experience";
+import Education from "./Education";
+import Contact from "./Contact";
+import "./App.css";
+import { Layout, Row, Col, Anchor, Typography } from "antd";
 
-function App() {
+const { Content, Sider } = Layout;
+const { Title } = Typography;
+const { Link } = Anchor;
+
+const TextLevelForNavigation = 5;
+const TitleStyleForNavigation = {
+  fontWeight: 100
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider style={{ padding: "20% 20px" }} theme="light">
+        <Anchor>
+          <Link
+            href="#about"
+            title={
+              <Title
+                level={TextLevelForNavigation}
+                style={TitleStyleForNavigation}
+              >
+                About
+              </Title>
+            }
+          />
+          <Link
+            href="#skills"
+            title={
+              <Title
+                level={TextLevelForNavigation}
+                style={TitleStyleForNavigation}
+              >
+                Skills
+              </Title>
+            }
+          />
+          <Link
+            href="#experience"
+            title={
+              <Title
+                level={TextLevelForNavigation}
+                style={TitleStyleForNavigation}
+              >
+                Experience
+              </Title>
+            }
+          />
+          <Link
+            href="#education"
+            title={
+              <Title
+                level={TextLevelForNavigation}
+                style={TitleStyleForNavigation}
+              >
+                Education
+              </Title>
+            }
+          />
+          <Link
+            href="#contact"
+            title={
+              <Title
+                level={TextLevelForNavigation}
+                style={TitleStyleForNavigation}
+              >
+                Contact
+              </Title>
+            }
+          />
+        </Anchor>
+      </Sider>
+      <Layout>
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, textAlign: "center" }}
+          >
+            <Row>
+              <Col
+                span={20}
+                offset={2}
+                style={{
+                  boxShadow:
+                    "0 0px 0px 0px white, 0 -0px 0px 0px white, 12px 0 15px -4px rgba(128,128,128, 0.8), -12px 0 15px -4px rgba(128,128,128, 0.8)"
+                }}
+              >
+                <About />
+                <Skills />
+                <Experience />
+                <Education />
+                <Contact />
+              </Col>
+            </Row>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
